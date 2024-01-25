1a88cd193f6d1dae313f127183d6ed75711b6f3a
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2024-01-25 12:50:08 +0200

    frontend: register form

--- project/src/views/RegisterView.vue
+++ project/src/views/RegisterView.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="main">
-    <form>
+    <form @submit.prevent="registerMethod" class="form-container">
       <h1>Register</h1>
-      <input type="email" placeholder="Email" class="input-field" />
-      <input type="text" placeholder="Username" class="input-field" />
-      <input type="password" placeholder="Password" class="input-field" />
-      <input type="password" placeholder="Confirm Password" class="input-field" />
+      <input
+        type="email"
+        placeholder="Email"
+        class="input-field"
+        v-model="email"
+      />
+      <input
+        type="text"
+        placeholder="Username"
+        class="input-field"
+        v-model="username"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="input-field"
+        v-model="password"
+      />
       <div class="login-btn">
         <button class="btn1">Register</button>
       </div>
@@ -13,12 +27,70 @@
         Have an account?
         <router-link to="/login" class="login-link">Login Here</router-link>
       </p>
+      <p>{{ message }}</p>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+import { requestOptions, base_url } from "@/utils/requestOptions";
+import router from "@/router";
+export default {
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+      message: "",
+    };
+  },
+  methods: {
+    registerMethod: function () {
+      console.log("Am intrat in registerMethod...");
+      console.log(
+        "Vrei sa faci register cu username",
+        this.username,
+        "si parola",
+        this.password,
+        "si email: ",
+        this.email
+      );
+
+      if (this.password === "" || this.email === "") {
+        this.message = "Parola sau emailul nu pot fi valori nule";
+      }
+
+      if (this.email && this.password) {
+        console.log("e ok");
+
+        let localRequestOptions = { ...requestOptions };
+        localRequestOptions.method = "POST";
+
+        let postData = {
+          email: this.email,
+          username: this.username,
+          password: this.password,
+        };
+
+        localRequestOptions.body = JSON.stringify(postData);
+
+        fetch(base_url + "register", localRequestOptions)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.message === "User added") {
+              router.replace("/login");
+            } else {
+              this.message = data.message;
+              this.email = "";
+              this.password = "";
+              this.username = "";
+            }
+          });
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -29,9 +101,12 @@ export default {};
   height: 100vh;
 }
 
-form {
+.form-container {
   max-width: 400px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 h1 {
@@ -63,6 +138,4 @@ h1 {
   text-decoration: underline;
   cursor: pointer;
 }
-
-
 </style>
