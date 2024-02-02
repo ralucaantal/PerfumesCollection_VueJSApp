d58ea9cdbf451539b0affce4a80cfea0893294de
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2024-02-02 23:24:55 +0200

    imi afiseaza corect numele brandului

--- project/src/views/AddUpdatePerfumeView.vue
+++ project/src/views/AddUpdatePerfumeView.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-   <h1>{{brandId}}</h1>
+    <h1> Perfumes from brand: {{ brandName }}</h1>
   </div>
 </template>
 
 <script>
-
+import { ref, onMounted } from "vue";
 import { useStore } from "vuex";
+
 export default {
   setup() {
-    
-  },
-
-  created() {
     const store = useStore();
-    console.log(store.state.brandId);
-    console.log(store.state.brandName);
+    const brandId = ref(null);
+    const brandName = ref(null);
+
+    onMounted(() => {
+      brandId.value = store.state.brandId;
+      brandName.value = store.state.brandName;
+    });
+
+    return { brandId, brandName };
   },
 };
 </script>
